046c3fa32ad2bb697d8fbb3021f8e73cf8eaf624
xeirniomJ <binri6267inj@126.com>
2021-04-19 15:16:09 +0800

    创建了项目

---
+++ oneApp/oneApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oneApp
+//
+//  Created by ioser on 17/4/22.
+//  Copyright © 2017年 ioser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oneApp/oneApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oneApp
+//
+//  Created by ioser on 17/4/22.
+//  Copyright © 2017年 ioser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
